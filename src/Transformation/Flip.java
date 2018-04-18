eb54477bec0284b934a0ee7252274c4b13d1da6a
Huỳnh Trương Nhựt <luminous997@gmail.com>
2018-04-18 16:49:01 +0700

    crop

--- src/Transformation/Flip.java
+++ src/Transformation/Flip.java
@@ -52,7 +52,6 @@ public class Flip extends AbstractImageAction {
     public Flip(BufferedImage originalImage, Orientation orientation) {
         super(originalImage);
         this.orientation = orientation;
-
         setName("Flip " + orientation.getName());
     }
 
