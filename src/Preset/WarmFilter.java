c656490b51d53b24f4444d679a96db66c841577d
Huỳnh Trương Nhựt <luminous997@gmail.com>
2018-05-10 16:42:28 +0700

    Added Preset.WarmFilter

---
+++ src/Preset/WarmFilter.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Preset;
+
+import Action.ImageColorAction;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author Admin
+ */
+public class WarmFilter extends ImageColorAction {
+
+    public WarmFilter(BufferedImage originalImage) {
+        super(originalImage);
+    }
+
+    @Override
+    protected Color convertColor(Color color) {
+        int red = color.getRed();
+        int blue = color.getBlue();
+        int temp = (int) (red * 0.5);
+        red += temp;
+        if (red > 255) {
+            red = 255;
+        }
+        blue -= temp;
+        if (blue < 0) {
+            blue = 0;
+        }
+
+        return new Color(red, color.getGreen(), blue);
+    }
+
+}
